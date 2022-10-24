82d0fb2f8836a2166a884f0f97b4803653da04f5
nhatthegreat777 <nhatvan1561@gmail.com>
2022-10-23 21:22:12 -0500

    update

--- lehmer_method/lehmer.c
+++ lehmer_method/lehmer.c
@@ -4,8 +4,9 @@
 //the first iteration is the seed
 //multip by a multiplier
 //module of a prime number
-uint32_t lcg_parkmiller(uint64_t *state, FILE *file)
+uint32_t lcg_parkmiller(uint64_t *state, int *item_count, FILE *file)
 {
+	//printf("first: %d\n", *item_count);
 	*state = *state * 48271 % 0x7fffffff;
 	int bin[64];
 	int temp = *state;
@@ -18,8 +19,11 @@ uint32_t lcg_parkmiller(uint64_t *state, FILE *file)
 	}
 	for (int i = 0; i <= 63; i++){
 		fprintf(file,"%d", bin[i]);
+		*item_count += 1;
+
+		//printf("second: %d\n", *item_count);
+		if (*item_count%25 == 0) fprintf(file,"\n");
 	}
-	fprintf(file,"\n");	
 	//printf("%ld", *state);
 	//printf("\n");
 	return *state;
@@ -28,10 +32,11 @@ uint32_t lcg_parkmiller(uint64_t *state, FILE *file)
 void main() {
 	uint64_t num = 10;
 	FILE *file;
+	int item_count = 0;
 	file = fopen("test.txt", "w+");
 	//generate 1million sequence
 	for (int i = 0; i <= 1000000; i++){
-		lcg_parkmiller(&num, file);
+		lcg_parkmiller(&num, &item_count, file);
 		//printf("%d\n", num);
 	
 	}
